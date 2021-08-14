7736bc3c3825ca51318c9edc44f1e71438438fcc
Waleed Yousaf <waleedyousaf@wanclouds.net>
2021-08-14 19:10:36 +0500

    uses aliasing type

---
+++ public/functions/aliasing.js
@@ -0,0 +1,8 @@
+"use strict";
+/***============== Functions ==============**/
+var logDetails = function (uid, item) {
+    console.log(item + " has a uid: " + uid);
+};
+var greeting = function (user) {
+    console.log((user === null || user === void 0 ? void 0 : user.name) + " says hello");
+};
---
+++ src/functions/aliasing.ts
@@ -0,0 +1,13 @@
+/***============== Functions ==============**/
+
+// can reuse types, to avoid duplication
+type StringOrNum = string | number;
+type objWithName = { name: string, uid: StringOrNum };
+
+const logDetails = (uid: StringOrNum, item: string) => {
+  console.log(`${item} has a uid: ${uid}`);
+};
+
+const greeting = (user: objWithName) => {
+  console.log(`${user?.name} says hello`);
+};
