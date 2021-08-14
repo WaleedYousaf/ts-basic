b38d10e4870b02415362f813b816d11a64378305
Waleed Yousaf <waleedyousaf@wanclouds.net>
2021-08-14 18:19:58 +0500

    strcutured the project and adds tsconfig

--- public/explicitTypes/explicitTypes.js
+++ public/explicitTypes/explicitTypes.js
@@ -1,19 +1,20 @@
+"use strict";
 /***============== Explicit Types ==============**/
 /***-------------- Basic types -----------------**/
-var character;
-var age;
-var isBlackBelt;
-// age = 'max'; TS dont allow cuz var age has an explicit type number
-isBlackBelt = true;
+var character2;
+var age2;
+var isBlackBelt2;
+// age2 = 'max'; TS dont allow cuz var age2 has an explicit type number
+isBlackBelt2 = true;
 /***-------------- Arrays -----------------**/
 // let ninjas: string[]; // not initialized, just defined type
 var ninjas = [];
 ninjas.push('adam');
 /***-------------- Union -----------------**/
-var mixed = [];
-mixed.push(20);
-mixed.push('ema');
-// mixed.push(false); // TS dont allow
+var mixed2 = [];
+mixed2.push(20);
+mixed2.push('ema');
+// mixed2.push(false); // TS dont allow
 // union on basic types (no need of `()`)
 var uid;
 uid = 123;
@@ -21,6 +22,6 @@ uid = 'maxie';
 // uid = false; // TS dont allow
 // union on object
 var ninjaOne;
-ninjaOne = { name: 'damian', age: 30 };
+ninjaOne = { name: 'damian', age2: 30 };
 var ninjaTwo;
-ninjaTwo = { name: 'matt', age: 19, beltColor: 'black' };
+ninjaTwo = { name: 'matt', age2: 19, beltColor: 'black' };
---
+++ public/main.js
@@ -0,0 +1,2 @@
+"use strict";
+console.log('base ts file');
