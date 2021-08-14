14e5e5ac458a9b24da6443c0d962eff5e4bc1989
waleedyousaf <44428566+WaleedYousaf@users.noreply.github.com>
2021-08-14 17:46:01 +0500

    Tut/Any (Dynamic) type (#6)
    * uses any type
    
    * updates readme
    
    Co-authored-by: Waleed Yousaf <waleedyousaf@wanclouds.net>

---
+++ anyType/anyType.js
@@ -0,0 +1,14 @@
+/***============== Any (Dynamic) Type ==============**/
+// reverses TS to JS (dont help in suggesting/forcing types
+var uid = 101;
+uid = '202';
+uid = {
+    name: 'max'
+};
+uid = {
+    myName: 'jill',
+    age: 20
+};
+var mixed = [];
+var ninja;
+ninja = { name: 'sa', age: 12 };
